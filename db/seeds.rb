5db25cfe48ff437a64df7554e3e37774c7e55039
Stensis <irenenjuguna98@gmail.com>
2022-10-19 22:12:39 +0300

    added files

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# puts " Seeding data..."
+
+# Article.create(title:"Why Deli Sandwiches Taste Better Than Yours" ,content:"A great sandwich is more than just meat, cheese and a schmear! There's even science behind why sandwiches tend to taste better when they're not made by you. (Long story short: When you make a sandwich, you imagine eating it, which actually makes you less hungry for it—more on this later.) We had a deli owner reveal the reasons why deli sandwiches are indeed so tasty. Plus, we've included tips on how you can replicate professionally made sandwiches at home.",category_id:1,author_id:1, comment_id:1,like_id:1,bookmark_id:1,date_posted:12/09/2022)  
+
+# Article.create(title:"Don’t Trash Your Old Phone—Give It a Second Life",content:"The original iPhone SE is a great little phone, and I love it. It has a headphone jack—remember those? It fits in a butt pocket. It was announced in the Obama era.
+# Sure, the first one I owned, which I purchased in 2017, had only 16GB of storage. And yes, I was forced to stop using it after a terrifying incident in which it refused to update to the latest iOS, even after I deleted nearly everything on it, which prevented me from installing the Ticketmaster app that I needed to enter a Harry Styles concert that I had flown to California by myself to attend. (Would you believe someone at the arena simply agreed to print the ticket out? I was crying.) After that, I bought a refurbished iPhone SE with 64GB of storage for $165. I eventually stopped using this one, because the camera was so bad that it was upsetting my friends. Also, a small part of the screen stopped working—right in the spot I had to press to switch the keyboard from letters to numbers, which meant I had no access to punctuation and came off, via text, as very cold. And I couldn’t log in to my bank account.",category_id:2,author_id:2, comment_id:1,like_id:1,bookmark_id:1,date_posted: )  
+
+# Article.create(title: ,content: ,category_id: ,author_id: , comment_id: ,like_id: ,bookmark_id: ,date_posted: )  
+# Article.create(title: ,content: ,category_id: ,author_id: , comment_id: ,like_id: ,bookmark_id: ,date_posted: )  
+# Article.create(title: ,content: ,category_id: ,author_id: , comment_id: ,like_id: ,bookmark_id: ,date_posted: )  
+
+
+# puts " Done seeding!"
