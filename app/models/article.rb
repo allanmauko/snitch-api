d3200a05aabf37150449696d3a4118614d6bbcf6
Stensis <irenenjuguna98@gmail.com>
2022-10-20 16:40:33 +0300

    changed files

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ActiveRecord::Base
     has_many :likes
     has_many :users, through: :likes
 
-    include SimpleRecommender::Recommendable
-    similar_by :users
+    # include SimpleRecommender::Recommendable
+    # similar_by :users
     
   end
