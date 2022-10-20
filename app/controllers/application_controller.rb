f5aaa3e0c24214ef594f44260f28aa334f0fe037
Daryl Rogony <darylrogony@gmail.com>
2022-10-20 22:51:47 +0300

    FIt: add routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/articleCategories" do
+    articleCategories = ArticleCategory.all
+    articleCategories.to_json
+  end
+
 end
