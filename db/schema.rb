273a340244f67ccc7a137e7133abf6a72e4db235
Daryl Rogony <darylrogony@gmail.com>
2022-10-20 20:33:34 +0300

    Fit: run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_183859) do
+ActiveRecord::Schema.define(version: 2022_10_20_172954) do
+
+  create_table "article_category", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +29,10 @@ ActiveRecord::Schema.define(version: 2022_10_19_183859) do
     t.datetime "date_posted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "author_id"
   end
 
 end
