c89c677f8a362ac9d9266d4b6ac50e7528c1504c
George <gnjuki19@gmail.com>
2022-10-23 13:51:20 +0300

    Added the categories table and pasted the routes in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,96 +1,134 @@
 class ApplicationController < Sinatra::Base
+
   set :default_content_type, 'application/json'
+
+  # USER  get "/users" do
+  #   users = User.all
+  #   users.to_json
+  # end
+
+
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # ARTICLE
+  get "/articles" do
+    articles = Article.all
+    articles.to_json
+  end
+
+  get "/articles" do
+    articles = Article.all
+    articles.to_json(include: :comments)
   end
 
 
-  #Comment routes:
+  post"/articles" do
+    new_article = Article.create(
+      title: params[:title],
+      content: params[:value],
+      image: params[:image],
+      number_of_likes: params[:number_of_likes],
+      category_id: params[:category_id],
+      author_id: params[:author_id]
+    )
+    new_article.to_json
+  end
 
+  patch"/articles/:id" do
+    article = Article.find(params[:id])
+    article.update(
+      title: params[:title],
+      content: params[:value],
+      image: params[:image],
+      number_of_likes: params[:number_of_likes],
+      category_id: params[:category_id],
+      author_id: params[:author_id]
+    )
+    article.to_json
+  end
+
+  delete"/articles" do
+    article = Article.find(params[:id])
+    article.destroy
+    article.to_json
+  end
+
+  # COMMENTS
   get "/comments" do
     comments = Comment.all
-
     comments.to_json
   end
 
-  get "/comments/:id" do
-    comment = Comment.find(params[:id])
-
-    comment.to_json
-  end
-
-
   post "/comments" do
-    comment = Comment.create(comment: params[:comment], 
-    article_id: params[:article_id], 
-    user_id: params[:user_id])
-
-    comment.to_json
-  end
-
-  patch "/comments/:id" do
-    comment = Comment.find(params[:id])
-    comment.update(
-      comment: params[:comment], 
-      article_id: params[:article_id], 
+    new_comment = Comment.create(
+      value: params[:value],
+      article_id: params[:article_id],
       user_id: params[:user_id]
     )
-
-    comment.to_json
+    new_comment.to_json
   end
 
+  patch"/comments/:id" do
+    comment = Comment.find(params[:id])
+    comment.update(
+      value: params[:value],
+    )
+    comment.to_json
+  end
 
   delete "/comments/:id" do
-    comment = Comment.find(params[:id])
-    comment.destroy
-    comment.to_json
+    comments = Comment.find(params[:id])
+    comments.destroy
+    comments.to_json
   end
 
-
-  # Likes routes
-
-  get "/likes" do
-    likes = Like.all
-
-    likes.to_json
+  # CATEGORY
+  get "/categories" do
+    category = Category.all
+    category.to_json
   end
 
-
-  get "/likes/:id" do
-    like = Like.find(params[:id])
-
-    like.to_json
+  post "/categories" do
+    new_category = Category.create(
+      name: params[:name],
+      author_id: params[:author_id]
+    )
+    new_category.to_json
   end
 
-  post "/likes" do
-    like = Like.create(count: params[:count],
-    user_id: params[:user_id],
-    article_id: params[:article_id])
-
-    like.to_json
+  patch"/categories/:id" do
+    category = Category.find(params[:id])
+    category.update(
+      value: params[:value],
+      )
+    category.to_json
   end
 
+  delete "/categories/:id" do
+    category = Category.find(params[:id])
+    category.destroy
+    category.to_json
+  end
 
-  patch "/likes/:id" do
-    like = Like.find(params[:id])
-    like.update(
-      count: params[:count],
+  # BOOKMARKS
+  get "/bookmarks" do
+    bookmark = Bookmark.all
+    bookmark.to_json
+  end
+
+  post "/bookmarks" do
+    saved_bookmark = Bookmark.create(
       user_id: params[:user_id],
       article_id: params[:article_id]
     )
+    saved_bookmark.to_json
+  end
 
-    like.to_json
+  delete "bookmarks/:id" do
+    bookmark = Bookmark.find(params[:id])
+    bookmark.destroy
+    bookmark.to_json
   end
 
 
-  delete "/likes/:id" do
-    like = Like.find(params[:id])
-    like.destroy
-
-    like.to_json
-  end
-  
 
 end
